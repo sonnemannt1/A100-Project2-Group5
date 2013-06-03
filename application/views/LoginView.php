bf2e5b12acab8942ae3d20bcdb090c7f27d6279b
Nicole Flokos <nicole.flokos@gmail.com>
2013-06-03 18:06:33 -0400

    aftermath of merge
    fixed some of the issues that occured as a result of today's merge of
    all of our projects.
    loginview.php had alot of html probs that we fixed.
    added javascript folders with scripts inside for better organization
    changed database.php password to be empty string so that i could log in.

--- application/views/LoginView.php
+++ application/views/LoginView.php
@@ -1,156 +1,28 @@
+<!DOCTYPE HTML>	
+	<html>
+
+	<head>
+		<title>Welcome to CTNextJobs!</title>
+			<link href='http://fonts.googleapis.com/css?family=Alef' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>static/css/styles.css">
+	<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"></script>
+	<script type="text/javascript" src="<?php echo base_url();?>/static/js/jquery.min.js"></script>
+	<script type="text/javascript" src="http://widgets.twimg.com/j/2/widget.js"></script>
+	<script type="text/javascript" src="<?php echo base_url();?>/static/js/javascript.js"></script>
+	
+	
+	
+	</head>
 	<?php
 
 		$this->load->helper("form");	
 
 	?>
-	<html>
-	<head>
-	<header><img src='http://newhiteboard.com/wp-content/uploads/2012/10/header.png'></header>
-	<link href='http://fonts.googleapis.com/css?family=Alef' rel='stylesheet' type='text/css'>
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>static/css/styles.css">
-	<title>Welcome to CTNextJobs!</title>
-	</head>
 
-	<body>
-
-	<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"></script>
-	<script type="text/javascript">
-
-
-		var currentTab = 0;
-
-		function Load() {
-		}
-
-		function tab_enter(tabID) {
-			if (tabID == 0) {
-				if (currentTab == 1 || currentTab == 2 || currentTab == 3) {
-					document.getElementById("firstTab").className = "tabHover";
-				return;
-				}
-			}
-			if (tabID == 1) {
-				if (currentTab == 0 || currentTab == 2 || currentTab == 3) {
-					document.getElementById("secondTab").className = "tabHover";
-			return;
-				}
-			}
-			if (tabID == 2) {
-				if (currentTab == 0 || currentTab == 1 || currentTab == 3){
-					document.getElementById("thirdTab").className = "tabHover";
-			return;
-				}
-			}
-		}
-
-		function tab_leave(tabID) {
-			if (currentTab == 0) {
-				document.getElementById("secondTab").className = "inactiveTab";
-				document.getElementById("thirdTab").className = "inactiveTab";
-			}
-			if (currentTab == 1) {
-				document.getElementById("firstTab").className = "inactiveTab";
-				document.getElementById("thirdTab").className = "inactiveTab";
-			}
-			if (currentTab == 2){
-				document.getElementById("firstTab").className = "inactiveTab";
-				document.getElementById("secondTab").className = "inactiveTab";
-			}
-		}
-
-<<<<<<< HEAD
-		function tab_click(tabID) {
-			// event handler for tabs
-			if (tabID == 0) { // tabID 0 indicates the first tab was clicked, so show the content in the first tab and set the current tab
-				if (currentTab != 0) {
-					currentTab = 0;
-					document.getElementById("firstTab").className = "selectedTab";
-					document.getElementById("secondTab").className = "inactiveTab";
-					document.getElementById("thirdTab").className = "inactiveTab";
-					document.getElementById("firstTabContent").className = "activeTabContent";
-					document.getElementById("secondTabContent").className = "inactiveTabContent";
-					document.getElementById("thirdTabContent").className = "inactiveTabContent";
-				}
-			}
-			if (tabID == 1) { // tabID 1 indicates the second tab was clicked, so show the content in the second tab and set the current tab
-				if (currentTab != 1) {
-					currentTab = 1;
-					document.getElementById("secondTab").className = "selectedTab";
-					document.getElementById("firstTab").className = "inactiveTab";
-					document.getElementById("thirdTab").className = "inactiveTab";
-					document.getElementById("secondTabContent").className = "activeTabContent";
-					document.getElementById("firstTabContent").className = "inactiveTabContent";
-					document.getElementById("thirdTabContent").className = "inactiveTabContent";
-				}
-			}
-			if (tabID == 2) { // tabID 2 indicates the third tab was clicked, so show the content in the third tab and set the current tab
-				if (currentTab != 2) {
-					currentTab = 2;
-					document.getElementById("thirdTab").className = "selectedTab";
-					document.getElementById("secondTab").className = "inactiveTab";
-					document.getElementById("firstTab").className = "inactiveTab";
-					document.getElementById("thirdTabContent").className = "activeTabContent";
-					document.getElementById("secondTabContent").className = "inactiveTabContent";
-					document.getElementById("firstTabContent").className = "inactiveTabContent";
-				}
-			}
-		}
-		
-	</script>
-=======
-function initialize() {
-	var mapOptions = {
-			zoom: 15,
-			center: new google.maps.LatLng(41.3081, -72.9286),
-			mapTypeId: google.maps.MapTypeId.ROADMAP
-	};
-
-	map = new google.maps.Map(document.getElementById('map-canvas'),
-			mapOptions);
-}
-
-google.maps.event.addDomListener(window, 'load', initialize);
-
-</script>
-<script src="http://widgets.twimg.com/j/2/widget.js"></script>
-<script>
-new TWTR.Widget({
-  version: 2,
-  type: 'profile',
-  rpp: 4,
-  interval: 6000,
-  width: 216,
-  height: 240,
-  theme: {
-    shell: {
-      background: 'lightblue',
-      color: '#292b2c'
-    },
-    tweets: {
-      background: 'lightgray',
-      color: '#292b2c',
-      links: '#1388d2'
-    }
-  },
-  features: {
-    scrollbar: true,
-    loop: false,
-    live: true,
-    hashtags: true,
-    timestamp: true,
-    avatars: false,
-    behavior: 'all'
-  }
-}).render().setUser('gridnewhaven').start();
-</script>
-
-<title>Login</title>
-</head>
 <body>
-
+	
+<header><img src='http://newhiteboard.com/wp-content/uploads/2012/10/header.png'></header>
 <table>
-<table>
-<table align=left>
 <tr>
 <td align=left valign="middle">
 	<div id="tabs" align="left">
@@ -191,84 +63,14 @@ new TWTR.Widget({
 		?>
 		</td>
 		</tr>
-		
+		</table>
 		<div id="fb-root">
-<script>
-  window.fbAsyncInit = function() {
-  FB.init({
-    appId      : '468413103234050', // App ID
-    channelUrl : 'http://localhost/A100-Project2-Group5/', // Channel File
-    status     : true, // check login status
-    cookie     : true, // enable cookies to allow the server to access the session
-    xfbml      : true  // parse XFBML
-  });
 
-  // Here we subscribe to the auth.authResponseChange JavaScript event. This event is fired
-  // for any authentication related change, such as login, logout or session refresh. This means that
-  // whenever someone who was previously logged out tries to log in again, the correct case below 
-  // will be handled. 
-  FB.Event.subscribe('auth.authResponseChange', function(response) {
-    // Here we specify what we do with the response anytime this event occurs. 
-    if (response.status === 'connected') {
-      // The response object is returned with a status field that lets the app know the current
-      // login status of the person. In this case, we're handling the situation where they 
-      // have logged in to the app.
-      testAPI();
-    } else if (response.status === 'not_authorized') {
-      // In this case, the person is logged into Facebook, but not into the app, so we call
-      // FB.login() to prompt them to do so. 
-      // In real-life usage, you wouldn't want to immediately prompt someone to login 
-      // like this, for two reasons:
-      // (1) JavaScript created popup windows are blocked by most browsers unless they 
-      // result from direct interaction from people using the app (such as a mouse click)
-      // (2) it is a bad experience to be continually prompted to login upon page load.
-      FB.login();
-    } else {
-      // In this case, the person is not logged into Facebook, so we call the login() 
-      // function to prompt them to do so. Note that at this stage there is no indication
-      // of whether they are logged into the app. If they aren't then they'll see the Login
-      // dialog right after they log in to Facebook. 
-      // The same caveats as above apply to the FB.login() call here.
-      FB.login();
-    }
-  });
-  };
->>>>>>> 5c1a1a28fc8feb309dd6bcc955f91b1371f48c7d
-
-	<script>
-	google.maps.visualRefresh = true;
-
-<<<<<<< HEAD
-	var map;
-
-	function initialize() {
-		var mapOptions = {
-				zoom: 15,
-				center: new google.maps.LatLng(41.3081, -72.9286),
-				mapTypeId: google.maps.MapTypeId.ROADMAP
-		};
-
-		map = new google.maps.Map(document.getElementById('map-canvas'),
-				mapOptions);
-	}
-
-	google.maps.event.addDomListener(window, 'load', initialize);
-=======
-  // Here we run a very simple test of the Graph API after login is successful. 
-  // This testAPI() function is only called in those cases. 
-  function testAPI() {
-    console.log('Welcome!  Fetching your information.... ');
-    FB.api('/me', function(response) {
-      console.log('Good to see you, ' + response.name + '.');
-    });
-  }
-</script>
 </div>
 <div>
 		<fb:login-button show-faces="true" width="200" max-rows="1"></fb:login-button>
 		</div>
-</table>
-</table>
+		
 
 	<div id="tabs" style="float:right; margin-right:10%;margin-top:-15%">
 		<div class="tabContainer">
@@ -285,9 +87,9 @@ new TWTR.Widget({
 	</div>
 <div id="firstTabContent" class="inactiveTabContent" style="float:right; margin-top:-15%" >
 			<div>
-		<?php  
-				echo ("<table>");
+		<?php  				
 				echo(form_open("LoginController/SubmitJobSeeker"));
+				echo ("<table>");
 				echo ("<tr>\n");
 				echo ("<td>\n");
 				echo ("First Name:");
@@ -359,12 +161,13 @@ new TWTR.Widget({
 				echo (form_input(array("id" => "telephone", "name" => "telephone", "type" => "text")));
 				echo ("</td>");
 				echo ("</tr>\n");
->>>>>>> 5c1a1a28fc8feb309dd6bcc955f91b1371f48c7d
+				echo ("</table>");
+				echo ("</form>");
 
-	</script>
+	?>
 	
 	<script src="http://widgets.twimg.com/j/2/widget.js"></script>
-	<script>
+	<script type="text/javascript">
 	new TWTR.Widget({
 	  version: 2,
 	  type: 'profile',
@@ -395,9 +198,8 @@ new TWTR.Widget({
 	}).render().setUser('gridnewhaven').start();
 	</script>
 	
-<<<<<<< HEAD
+
 	<table>
-	<table align=left>
 	<tr>
 	<td align=left valign="middle">
 		<div id="tabs" align="left">
@@ -422,7 +224,7 @@ new TWTR.Widget({
 				<table style="position: relative; width: 100%;">
 					<tr>
 					<td align=center valign=middle>
-=======
+					<?php
 				echo ("<tr>\n");
 				echo ("<td colspan=2>\n");
 				echo (form_submit(array("name" => "submit", "value" => "Submit", "style" => "width: 47%; margin-right: 3%")));
@@ -430,7 +232,6 @@ new TWTR.Widget({
 				echo ("</td>\n");
 				echo ("</tr>\n");
 				echo ("</table>");
-				echo (form_close() . "\n");
 
 				?>
 				
@@ -439,8 +240,8 @@ new TWTR.Widget({
 		<div id="secondTabContent" class="inactiveTabContent" style="float:right; margin-top:-15%" >
 			<div style="margin-top: 10px;">
 			<?php  
-				echo ("<table>");
 				echo(form_open("LoginController/SubmitJobPoster"));
+				echo ("<table>");
 				echo ("<tr>\n");
 				echo ("<td>\n");
 				echo ("Company Name:");
@@ -449,10 +250,12 @@ new TWTR.Widget({
 				echo (form_input(array("id" => "companyName", "name" => "companyName", "type" => "text")));
 				echo ("</td>");
 				echo ("</tr>\n");
->>>>>>> 5c1a1a28fc8feb309dd6bcc955f91b1371f48c7d
+				echo ("</table>");
+				echo ("</form>");
+				?>
 					
 	<div id="firstTabContent">
-	<script>
+	<script  type="text/javascript">
 	  window.fbAsyncInit = function() {
 	  FB.init({
 		appId      : '468413103234050', // App ID
@@ -534,18 +337,19 @@ new TWTR.Widget({
 
 			?>
 			
-			</td>
+			<!-- </td>
 			<td>
 			</td>
 			</tr>
-			</table>
+			</table> -->
 			</div>
 			</div>
 			<div id="secondTabContent" class="inactiveTabContent">
 				<div>
-			<?php  
-					echo ("<table>");
+			<?php 
 					echo(form_open("LoginController/SubmitJobSeeker"));
+					echo ("<table>");
+					
 					echo ("<tr>\n");
 					echo ("<td>\n");
 					echo ("First Name:");
@@ -663,8 +467,9 @@ new TWTR.Widget({
 			<div id="thirdTabContent" class="inactiveTabContent">
 				<div style="margin-top: 10px;">
 				<?php  
-					echo ("<table>");
 					echo(form_open("LoginController/SubmitJobPoster"));
+					echo ("<table>");
+					
 					echo ("<tr>\n");
 					echo ("<td>\n");
 					echo ("Company Name:");
@@ -740,7 +545,6 @@ new TWTR.Widget({
 					echo ("</td>");
 					echo ("</tr>\n");
 
-<<<<<<< HEAD
 					
 					echo ("<tr>\n");
 					echo ("<td>\n");
@@ -765,20 +569,18 @@ new TWTR.Widget({
 			</div>
 
 	</div>
-	</table>
+	
 	<div id="map-canvas" style="width:25%; height:25%; margin-top: -10%; float:right">
 	</div>
 	</table>
-=======
-				?>
+
 		</div>
 		</div>
 		</div>
 		</table>
 		<div id="map-canvas" style="width:40%; height:40%; margin-top: -15%; float:middle; margin-left:45%">
 </div>
-</table>
->>>>>>> 5c1a1a28fc8feb309dd6bcc955f91b1371f48c7d
+
 
 	</body>
 	</html>
