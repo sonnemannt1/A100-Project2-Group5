ac1489604f1191c9a52f13c01de4f08a4ca83504
Nicole Flokos <nicole.flokos@gmail.com>
2013-06-04 10:19:28 -0400

    aftermath of merge pt 2

--- application/views/LoginView.php
+++ application/views/LoginView.php
@@ -3,6 +3,7 @@
 
 	<head>
 		<title>Welcome to CTNextJobs!</title>
+		
 			<link href='http://fonts.googleapis.com/css?family=Alef' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>static/css/styles.css">
 	<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"></script>
@@ -20,7 +21,6 @@
 	?>
 
 <body>
-	
 <header><img src='http://newhiteboard.com/wp-content/uploads/2012/10/header.png'></header>
 <table>
 <tr>
@@ -255,67 +255,6 @@
 				?>
 					
 	<div id="firstTabContent">
-	<script  type="text/javascript">
-	  window.fbAsyncInit = function() {
-	  FB.init({
-		appId      : '468413103234050', // App ID
-		channelUrl : 'http://localhost/A100-Project2-Group5/', // Channel File
-		status     : true, // check login status
-		cookie     : true, // enable cookies to allow the server to access the session
-		xfbml      : true  // parse XFBML
-	  });
-
-	  // Here we subscribe to the auth.authResponseChange JavaScript event. This event is fired
-	  // for any authentication related change, such as login, logout or session refresh. This means that
-	  // whenever someone who was previously logged out tries to log in again, the correct case below 
-	  // will be handled. 
-	  FB.Event.subscribe('auth.authResponseChange', function(response) {
-		// Here we specify what we do with the response anytime this event occurs. 
-		if (response.status === 'connected') {
-		  // The response object is returned with a status field that lets the app know the current
-		  // login status of the person. In this case, we're handling the situation where they 
-		  // have logged in to the app.
-		  testAPI();
-		} else if (response.status === 'not_authorized') {
-		  // In this case, the person is logged into Facebook, but not into the app, so we call
-		  // FB.login() to prompt them to do so. 
-		  // In real-life usage, you wouldn't want to immediately prompt someone to login 
-		  // like this, for two reasons:
-		  // (1) JavaScript created popup windows are blocked by most browsers unless they 
-		  // result from direct interaction from people using the app (such as a mouse click)
-		  // (2) it is a bad experience to be continually prompted to login upon page load.
-		  FB.login();
-		} else {
-		  // In this case, the person is not logged into Facebook, so we call the login() 
-		  // function to prompt them to do so. Note that at this stage there is no indication
-		  // of whether they are logged into the app. If they aren't then they'll see the Login
-		  // dialog right after they log in to Facebook. 
-		  // The same caveats as above apply to the FB.login() call here.
-		  FB.login();
-		}
-	  });
-	  };
-
-	  // Load the SDK asynchronously
-	  (function(d){
-	   var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
-	   if (d.getElementById(id)) {return;}
-	   js = d.createElement('script'); js.id = id; js.async = true;
-	   js.src = "//connect.facebook.net/en_US/all.js";
-	   ref.parentNode.insertBefore(js, ref);
-	  }(document));
-
-	  // Here we run a very simple test of the Graph API after login is successful. 
-	  // This testAPI() function is only called in those cases. 
-	  function testAPI() {
-		console.log('Welcome!  Fetching your information.... ');
-		FB.api('/me', function(response) {
-		  console.log('Good to see you, ' + response.name + '.');
-		});
-	  }
-	</script>
-	<fb:login-button show-faces="true" width="200" max-rows="1"></fb:login-button>
-	</div>
 			<?php 
 
 			echo form_open("/LoginController/CheckValidLogin");
@@ -570,15 +509,8 @@
 
 	</div>
 	
-	<div id="map-canvas" style="width:25%; height:25%; margin-top: -10%; float:right">
-	</div>
-	</table>
+    <div id="map-canvas"></div>
 
-		</div>
-		</div>
-		</div>
-		</table>
-		<div id="map-canvas" style="width:40%; height:40%; margin-top: -15%; float:middle; margin-left:45%">
 </div>
 
 
